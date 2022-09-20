4b042a609ca61391ed4d935095ae671fe2a25a43
boqianlv <3049344036@qq.com>
2022-09-20 16:20:49 +0900

    phpinfo

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
+Route::get('phpinfo',function(){
+    return phpinfo();
+});
