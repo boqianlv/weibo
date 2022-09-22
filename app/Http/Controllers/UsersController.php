7b9d9c0b2da2054eb6a61313723614967b05cf81
boqianlv <3049344036@qq.com>
2022-09-22 15:16:12 +0900

    用户激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class UsersController extends Controller
 {
@@ -12,12 +13,24 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show','create','store','index']
+            'except' => ['show','create','store','index','confirmEmail']
         ]);
 
         $this->middleware('guest',[
             'only' => ['create']
         ]);
+    }
+    public function confirmEmail($token)
+    {
+        $user=User::where('activation_token',$token)->firstOrFail();
+
+        $user->activated=true;
+        $user->activation_token=null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success','恭喜你哦，激活成功！');
+        return redirect()->route('users.show',[$user]);
     }
     public function create()
     {
@@ -39,9 +52,25 @@ class UsersController extends Controller
             'email'=>$request->email,
             'password'=>bcrypt($request->password),
         ]);
-        Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程。');
-        return redirect()->route('users.show',[$user]);
+        //Auth::login($user);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success','验证邮件已发送到你的注册邮箱上，请注意查收。');
+        //return redirect()->route('users.show',[$user]);
+        return redirect('/');
+    }
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view='emails.confirm';
+        $data=compact('user');
+        $from='summer@example.com';
+        $name='Summer';
+        $to=$user->email;
+        $subject="感谢您注册Weibo应用！请确认你的邮箱";
+
+        Mail::send($view,$data,function($message) use ($from,$name,$to,$subject){
+            $message->from($from,$name)->to($to)->subject($subject);
+        });
+
     }
     public function edit(User $user)
     {
