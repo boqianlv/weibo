b5c445546868166cf910727f5c02cc262290f314
boqianlv <3049344036@qq.com>
2022-09-23 10:33:50 +0900

    移除邮件发送的from选项

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -13,12 +13,12 @@ class PasswordController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('throttle:2,1',[
-            'only' => ['showLinkRequestForm']
-        ]);
-        $this->middleware('throttle:3,10',[
-            'only' => ['sendResetLinkEmail']
-        ]);
+        // $this->middleware('throttle:2,1',[
+        //     'only' => ['showLinkRequestForm']
+        // ]);
+        // $this->middleware('throttle:3,10',[
+        //     'only' => ['sendResetLinkEmail']
+        // ]);
     }
     public function showLinkRequestForm(){
         return view('auth.passwords.email');
