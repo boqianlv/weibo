0f7c251d646afd3a94f5ab116600ebf97f961d32
boqianlv <3049344036@qq.com>
2022-09-27 10:46:21 +0900

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,32 @@ class User extends Authenticatable
         return $this->statuses()->orderby('created_at','desc');
 
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+    public function follow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids=compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    public function isFollowing($user_id)
+    {
+        return $this->following->contains($user_id);
+    }
 }
